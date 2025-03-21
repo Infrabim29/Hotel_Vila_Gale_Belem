a569b014d5f3405571a9b5acafb0b32ce926624c
Infrabim29 <infrabim@outlook.com>
2025-03-20 21:26:20 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,437 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-central",
+      "name": "CENTRAL",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.614991376787632,
+        "pitch": 0.797495933393737,
+        "fov": 1.3473765980835988
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.746401873960287,
+          "pitch": 0.045383053984746624,
+          "rotation": 3.141592653589793,
+          "target": "1-sul"
+        },
+        {
+          "yaw": -2.4805350132929505,
+          "pitch": 0.059811528773263234,
+          "rotation": 3.141592653589793,
+          "target": "8-sudoeste"
+        },
+        {
+          "yaw": -0.5476947504665155,
+          "pitch": 0.006624334004508015,
+          "rotation": 3.141592653589793,
+          "target": "2-norte"
+        },
+        {
+          "yaw": 0.3672851645415669,
+          "pitch": 0.030359241700574913,
+          "rotation": 3.141592653589793,
+          "target": "3-nordeste"
+        },
+        {
+          "yaw": -1.192963351931887,
+          "pitch": 1.1779972216161774,
+          "rotation": 6.283185307179586,
+          "target": "4-centro_inferior"
+        },
+        {
+          "yaw": -0.4663570591249737,
+          "pitch": 0.8139311849717998,
+          "rotation": 12.566370614359176,
+          "target": "5-entre_edificaes"
+        },
+        {
+          "yaw": -0.24523591677021273,
+          "pitch": 0.4353733826259205,
+          "rotation": 9.42477796076938,
+          "target": "6-norte_edificaes"
+        },
+        {
+          "yaw": -1.5914398267637893,
+          "pitch": 0.05089541205610004,
+          "rotation": 3.141592653589793,
+          "target": "7-oeste"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sul",
+      "name": "SUL",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.05369210425042681,
+          "pitch": -0.05732116670229459,
+          "rotation": 3.141592653589793,
+          "target": "0-central"
+        },
+        {
+          "yaw": -1.3270823962208524,
+          "pitch": 0.07587028339083446,
+          "rotation": 2.356194490192345,
+          "target": "8-sudoeste"
+        },
+        {
+          "yaw": -0.8006086343773013,
+          "pitch": -0.03136346253305433,
+          "rotation": 3.141592653589793,
+          "target": "7-oeste"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-norte",
+      "name": "NORTE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.02647539318777703,
+          "pitch": -0.03814153347404847,
+          "rotation": 3.141592653589793,
+          "target": "0-central"
+        },
+        {
+          "yaw": -0.9319817802666694,
+          "pitch": 0.08602360601870451,
+          "rotation": 3.141592653589793,
+          "target": "3-nordeste"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-nordeste",
+      "name": "NORDESTE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.9416161173362632,
+        "pitch": 0.6441128510563594,
+        "fov": 1.3473765980835988
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1390833132905094,
+          "pitch": 0.02882907412082325,
+          "rotation": 3.141592653589793,
+          "target": "0-central"
+        },
+        {
+          "yaw": -1.6405387727139615,
+          "pitch": 0.11672903510238797,
+          "rotation": 3.141592653589793,
+          "target": "1-sul"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-centro_inferior",
+      "name": "CENTRO_INFERIOR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.3063504240773014,
+        "pitch": 0.07200081565881078,
+        "fov": 1.3473765980835988
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.09949122248258391,
+          "pitch": -0.14924526724087528,
+          "rotation": 3.141592653589793,
+          "target": "5-entre_edificaes"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-entre_edificaes",
+      "name": "ENTRE_EDIFICAÇÕES",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -3.1293591272513055,
+        "pitch": 0.08781021112438303,
+        "fov": 1.3473765980835988
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.117757074204654,
+          "pitch": -0.1296137800566335,
+          "rotation": 3.141592653589793,
+          "target": "6-norte_edificaes"
+        },
+        {
+          "yaw": -0.12933305109844007,
+          "pitch": -0.6496297965457245,
+          "rotation": 3.141592653589793,
+          "target": "0-central"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-norte_edificaes",
+      "name": "NORTE_EDIFICAÇÕES",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.0017700276340342214,
+        "pitch": -0.03726151928750454,
+        "fov": 1.3473765980835988
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.04878143493214182,
+          "pitch": -0.45410895790132244,
+          "rotation": 3.141592653589793,
+          "target": "0-central"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-oeste",
+      "name": "OESTE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.06294805440351325,
+        "pitch": 0.11034157158411517,
+        "fov": 1.3473765980835988
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.053689725287132006,
+          "pitch": -0.3867983025739772,
+          "rotation": 3.141592653589793,
+          "target": "0-central"
+        },
+        {
+          "yaw": -1.5122616427459725,
+          "pitch": -0.1935664889471731,
+          "rotation": 3.141592653589793,
+          "target": "2-norte"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-sudoeste",
+      "name": "SUDOESTE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.07116005930158664,
+        "pitch": 0.2828509855589676,
+        "fov": 1.3473765980835988
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.15325463677041284,
+          "pitch": -0.2173271256112841,
+          "rotation": 3.141592653589793,
+          "target": "0-central"
+        },
+        {
+          "yaw": -0.908768134394137,
+          "pitch": 0.14945480357851437,
+          "rotation": 7.853981633974483,
+          "target": "7-oeste"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
